e7f002e5924cb68d39ad3a97d2cf53f9ccad663f
Arjan Egges <arjan@quarterfall.com>
2023-01-23 12:34:49 +0100

    Added code examples.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# 2023-decorator
+# Python Decorators: The Complete Guide
+
+Python decorators are a great way to add functionality to your Python functions. In this video, I'll show you what they are, how they work, and some of the most useful decorators you can use in your code.
+
+Video: https://youtu.be/QH5fw9kxDQA.
---
+++ decorator_pattern_classic.py
@@ -0,0 +1,66 @@
+import logging
+from abc import ABC, abstractmethod
+from math import sqrt
+from time import perf_counter
+
+
+def is_prime(number: int) -> bool:
+    if number < 2:
+        return False
+    for element in range(2, sqrt(number) + 1):
+        if number % element == 0:
+            return False
+    return True
+
+
+class AbstractComponent(ABC):
+    @abstractmethod
+    def execute(self, upper_bound: int) -> int:
+        pass
+
+
+class AbstractDecorator(AbstractComponent):
+    def __init__(self, decorated: AbstractComponent) -> None:
+        self._decorated = decorated
+
+
+class ConcreteComponent(AbstractComponent):
+    def execute(self, upper_bound: int) -> int:
+
+        count = 0
+        for number in range(upper_bound):
+            if is_prime(number):
+                count += 1
+        return count
+
+
+class BenchmarkDecorator(AbstractDecorator):
+    def execute(self, upper_bound: int) -> int:
+        start_time = perf_counter()
+        value = self._decorated.execute(upper_bound)
+        end_time = perf_counter()
+        run_time = end_time - start_time
+        logging.info(
+            f"Execution of {self._decorated.__class__.__name__} took {run_time:.2f} seconds."
+        )
+        return value
+
+
+class LoggingDecorator(AbstractDecorator):
+    def execute(self, upper_bound: int) -> int:
+        logging.info(f"Calling {self._decorated.__class__.__name__}")
+        value = self._decorated.execute(upper_bound)
+        logging.info(f"Finished {self._decorated.__class__.__name__}")
+        return value
+
+
+def main() -> None:
+    logging.basicConfig(level=logging.INFO)
+    component = ConcreteComponent()
+    component = LoggingDecorator(component)
+    component = BenchmarkDecorator(component)
+    component.execute(50000)
+
+
+if __name__ == "__main__":
+    main()
